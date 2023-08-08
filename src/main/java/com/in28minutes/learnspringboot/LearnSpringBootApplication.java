95b570e0d2804e77aa4c70d08e281cfefd1b8355
levian89 <levi.gabrian@gmail.com>
2023-08-08 15:33:13 +0300

    Initial commit

---
+++ src/main/java/com/in28minutes/learnspringboot/LearnSpringBootApplication.java
@@ -0,0 +1,17 @@
+package com.in28minutes.learnspringboot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootConfiguration
+//@EnableAutoConfiguration
+//@ComponentScan
+@SpringBootApplication
+public class LearnSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootApplication.class, args);
+	}
+
+}
